f4a603d6ff0e8382764bab0940279407554ee235
Henze Fernandes <henzefernandes1974@gmail.com>
2026-03-23 11:56:05 -0300

    Add files via upload

---
+++ ListaDeExercicio/PilhaArray.java
@@ -0,0 +1,40 @@
+public class PilhaArray{
+    private int capacidade;
+    private Object[] a;
+    private int t;
+    private int FC;
+    public PilhaArray(int capacidade, int
+            crescimento) {
+        this.capacidade = capacidade;
+        t = -1;
+        FC = crescimento;
+        if (crescimento <= 0)
+            FC = 0;
+        a = new Object[capacidade];
+    }
+    public void push(Object o) {
+        if(t>=capacidade-1){
+            if(FC==0)
+                capacidade*=2;
+            else
+                capacidade+=FC;
+            Object[]  b=new Object[capacidade];
+            for(int f=0;f<a.length;f++)
+                b[f]=a[f];
+            a=b;
+        }
+        a[++t]=o;
+    }
+    public Object pop()throws PilhaVaziaExcecao {
+        if(isEmpty())
+            throw new PilhaVaziaExcecao("A Pilha está vazia" );
+        return a[t--];
+    }
+    public boolean isEmpty(){
+        return t==-1;
+    }
+
+    public int size(){
+        return t+1;
+    }
+}
---
+++ ListaDeExercicio/testePilha.java
@@ -0,0 +1,17 @@
+
+
+public class testePilha {
+
+	public static void main(String[] args) {
+		long inicio = System.currentTimeMillis();
+		Integer[] b = new Integer[1];		
+		PilhaArray pp=new PilhaArray(1,10);
+		System.out.println("inserindo");
+		for(int f=0;f<1000000;f++){
+		  pp.push(f);
+		}
+		long fim = System.currentTimeMillis();
+		long merda = fim - inicio;
+		System.out.println(merda);
+    }
+}
